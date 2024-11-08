01c87184aa2125073708b7d402ce4e4d695f424a
valesco19 <valesco19@gmail.com>
2024-11-07 16:59:32 -0800

    Update ncaab_game_deploy6.js

--- ncaab_game_deploy5.js
+++ ncaab_game_deploy5.js
@@ -1167,7 +1167,7 @@ function updateGameState(game_state_dict) {
 }
 
 function startWebSocket() {
-    ws_conn = new WebSocket('ws://localhost:3030');
+    ws_conn = new WebSocket('wss://api.untouted.com');
     console.log("Websocket Connected.");
 
     ws_conn.onmessage = function incoming(event) {
